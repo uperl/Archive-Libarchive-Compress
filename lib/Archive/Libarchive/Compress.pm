d2e2f523096015402ff5340a5800d10d92ee98eb
Graham Ollis <plicease@cpan.org>
2024-11-15 08:59:50 -0700

    Initial structure

---
+++ lib/Archive/Libarchive/Compress.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Archive::Libarchive::Compress {
+
+  # ABSTRACT: Recursively compress a directory (using libarchive)
+}
+
+1;
+
